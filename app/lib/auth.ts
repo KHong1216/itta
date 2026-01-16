6f982f255938ed5368fa46f0d6ea569812d227df
KHong1216 <rmsghd1216@naver.com>
2026-01-16 12:38:52 +0900

    Update auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -32,7 +32,7 @@ export async function signUpWithEmail(
         password,
         options: {
             data: {
-                nickname: trimmedNickname,
+                nickname: trimmedNickname,ㄹ
             },
         },
     });
